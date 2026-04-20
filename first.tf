e9f053d30df853f989a7fea010cc6200fa1ac9d7
Riddhi <riddhi.gore1910@gmail.com>
2026-04-21 01:31:10 +0530

    added first file

--- first.tf
+++ first.tf
@@ -19,6 +19,9 @@ output "hello1" {
 //  git config --global user.name "Riddhi"
 // git push
 //  git push --set-upstream origin first_branch
+
+// https://app.terraform.io/app/riddhi-org/workspaces/new
+
 //to run to this program  go to folder run below command 
 // terraform plan 
 // terraform validate
